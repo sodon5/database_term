0714dcdc8d4d1d1c8009940194ced689ffc9be4e
sodon5 <sodon513@gmail.com>
2017-06-26 15:51:32 +0900

    Update phpconnect.php

--- phpconnect.php
+++ phpconnect.php
@@ -1,20 +1,23 @@
+
 <?php
-	$con=mysqli_connect("localhost","root","","") or die ("MySQL 접속 실패!");
+	$con=mysqli_connect("localhost","root","","dbterm") or die ("MySQL 접속 실패!");
 //db연결		
 	$sql="
-	//query 적는 곳
+	select * from student 
 	";
 	
 	
 	$ret = mysqli_query($con,$sql);
 	
 	if($ret){
-		echo "table이 성공적으로 생성됨.";
+		echo mysqli_num_rows($ret), "건이 조회됨 <br><br>";
 	}
 	else{
 		echo "table 생성 실패!!"."<br>";
 		echo "실패 원인 : ".mysqli_error($con);
 	}
-
+	while($row = mysqli_fetch_array($ret)){
+		echo $row['studID'], " ", $row['studNAME'], " ", $row['studNUM'],"<br>";
+	}
 	mysqli_close($con);
 ?>
