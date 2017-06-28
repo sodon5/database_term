3599ad555f01cd3aac81a9c49ada4527e9476cd6
sodon5 <sodon513@gmail.com>
2017-06-28 13:01:36 +0900

    Add files via upload

---
+++ registrater/register_user.php
@@ -0,0 +1,14 @@
+<?php
+    header("Content-Type: text/html;charset=UTF-8");
+    $conn = mysqli_connect("localhost","root","1234","dbterm");   
+    $data_stream = "'".$_GET['Id']."','".$_GET['Password']."','".$_GET['Name']."','".$_GET['Email']."'";
+    $query = "insert into student(`studID`, `studPASSWORD`, `studNAME`, `studEMAIL`) values (".$data_stream.")";
+    $result = mysqli_query($conn, $query);
+     
+    if($result)
+      echo "1";
+    else
+      echo "-1";
+     
+    mysqli_close($conn);
+?>
